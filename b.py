89efc538b2b140381f31304e1849a47fc9fbf48c
artem <artemvasenko09@mail.ru>
2023-03-04 23:19:01 +0300

    Добавил коммнетов и requirements.txt

--- b.py
+++ b.py
@@ -1,5 +1,6 @@
 import pyperclip as clipboard
-msg = clipboard.paste()
+
+msg = clipboard.paste() # не перемещать <- получение ответа из буфера обмена перебивается коммандой ниже
 
 str_to_char = lambda string: [ord(i)+len(string) for i in string] # функция перевода в список 
 char_to_str = lambda char_list, string='': string.join([chr(i-len(char_list)) for i in char_list]) # функция перевода из списка в строку
@@ -10,7 +11,7 @@ print('Расшифровка:', char_to_str(l_msg))
 
 s_msg = str(input('Сообщение: '))
 if s_msg != '':
-    clipboard.copy(str((str_to_char(s_msg))))
+    clipboard.copy(str((str_to_char(s_msg)))) # если сообщение не пустое то мы его коопируем в буфер
 
 
 
